d5a0784ca07d6df9c0efe3a64cfd8ed2e9152e58
bielzin42 <nickspoot@gmail.com>
2020-07-04 19:39:36 -0300

    adding ft_sum, a function that adds an integer number to another.

---
+++ math/sum/ft_sum.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//function that adds :)
+void main (){
+	
+	int ft_sum(int firstValue, int secondValue){
+		
+		return firstValue + secondValue;
+	}
+
+	printf("The math result is: %d\n",ft_sum(10,10));
+
+}
