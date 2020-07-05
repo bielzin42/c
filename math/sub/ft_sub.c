8eaf11907a5b2c14904245ca449e4f2b1e9a98cb
bielzin42 <nickspoot@gmail.com>
2020-07-04 21:15:48 -0300

    uploading a new subtraction function outside an main!

---
+++ math/sub/ft_sub.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//function that does a subtraction of two values outside an main.
+
+	int ft_sub(int firstValue, int secondValue){
+		
+		return firstValue - secondValue;
+	}
+
---
+++ math/sub/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//function declaration
+int ft_sub(int firstValue, int secondValue);
+
+void main(){
+	
+	//calling the function that does the subtraction
+	printf("The value of the subtraction is %d: \n", 
+			ft_sub(10,5));
+
+}
