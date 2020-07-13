19aed88dc34f12dc8d7e3165cbce428ffe47bdd7
bielzin42 <nickspoot@gmail.com>
2020-07-13 14:38:13 -0300

    adding two new functions to the calculator

--- Sources/Cim/operations.h
+++ Sources/Cim/operations.h
@@ -10,27 +10,39 @@
 /* ************************************************************************** */
 
 
-//operation that sum two values
+//operation that sum two values.
 double ft_sum(double firstValue, double secondValue)
 {
 	return firstValue + secondValue;
 }
 
 
-//operation that subtract one value from another a - b
+//operation that subtract one value from another a - b.
 double ft_sub(double firstValue, double secondValue)
 {
 	return firstValue - secondValue;
 }
 
-//operation that multiplies two values a * b
+//operation that multiplies two values a * b.
 double ft_mult(double firstValue, double secondValue)
 {
 	return firstValue * secondValue;
 }
 
-//operation that divides one value bt another a / b
+//operation that divides one value bt another a / b.
 double ft_div(double firstValue, double secondValue)
 {
 	return firstValue / secondValue;
+}
+
+//operation that shows the rest of an division.
+int ft_rest(int firstValue, int secondValue)
+{
+	return firstValue % secondValue;
+}
+
+//operation that shows the absolute value of a number.
+int ft_absolute(int value)
+{
+	return abs(value);
 }
